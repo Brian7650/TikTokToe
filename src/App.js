4215c23f20e63836955706a9ba6bb87b5d0226ac
Brian.B <brtl765@gmail.com>
2023-03-21 13:25:15 -0400

    add tiktiktoe files

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
-import logo from './logo.svg';
+// import logo from './components/logos/logo.png';
 import './App.css';
+import TikTokToe from "./TikTokToe";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       
+        {/* <img src={logo} className="App-logo" alt="logo" />  */}
+        <div className="tiktoktoename">Tik.Tok.Toe</div>
+        <TikTokToe />
       </header>
     </div>
   );
